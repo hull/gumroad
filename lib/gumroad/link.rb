494f706a5ddd317043c190358ac666882a319ca0
Stephane Bellity <sbellity@gmail.com>
2013-04-14 02:31:16 +0200

    Adding link attributes...

--- lib/gumroad/link.rb
+++ lib/gumroad/link.rb
@@ -1,16 +1,34 @@
 module Gumroad
   class Link
-    attr_accessor :id, :name, :url, :description, :price
+
+    LinkAttributes = [
+      :id, 
+      :name, 
+      :url, 
+      :description, 
+      :price,
+      :preview, 
+      :country_available, 
+      :max_purchase_count, 
+      :customizable_price, 
+      :webhook, 
+      :require_shipping, 
+      :shown_on_profile, 
+      :custom_receipt, 
+      :custom_product_type
+    ]
+
+    attr_accessor LinkAttributes
     
     def initialize(session, json)
       @session = session
-      [:id, :name, :url, :description, :price].each do |attribute|
+      LinkAttributes.each do |attribute|
         instance_variable_set(:"@#{attribute}", json[attribute.to_s])
       end
     end
 
     def save
-      params = {name: name, url: url, description: description, price: price}
+      params = LinkAttributes.inject({}) { |prms, p| prms.merge(p => send(p)) }
       @session.post("/links", params)
       self
     end
